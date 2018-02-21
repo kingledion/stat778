b7aeded6a0552ac08b21a87603295f907ab7258b
Dan Hartig <kingledion@gmail.com>
2018-02-21 18:30:57 -0500

    final assignment, as submitted

--- hw1/hw1_graph.py
+++ hw1/hw1_graph.py
@@ -6,6 +6,10 @@ Created on Mon Feb 12 18:43:06 2018
 @author: dhartig
 """
 
+# Runs the hw1 executable in a subprocess, captures the output to stdout to get the data
+# then plots a graph for part c of HW1
+
+
 import subprocess, numpy as np
 from matplotlib import pyplot as plt
 
@@ -24,9 +28,9 @@ for row in complete.stdout.decode("utf-8").splitlines():
 vals = np.array(values)
     
 # 95% confidence interval is 1.96*sqrt(variance)
-variances = np.sqrt(np.array(variances)) * 1.96
-err_hi = vals + variances
-err_lo = vals - variances
+variances = np.sqrt(variances) * 1.96
+err_hi = vals**np.exp(np.divide(variances,(vals*np.log(vals))))
+err_lo = vals**np.exp(np.divide(-1*variances,(vals*np.log(vals))))
 
 plt.step(times, vals, 'b-', times, err_hi, 'r-', times, err_lo, 'r-')
 plt.show()
