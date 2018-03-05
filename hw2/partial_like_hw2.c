bc074956bb0a8601a672c3fe237df37411badd69
Dan Hartig <kingledion@gmail.com>
2018-03-05 18:36:28 -0500

    hw2 setup

---
+++ hw2/partial_like_hw2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// Structure to store tuples of data
+struct tuple {
+    double timeval; 
+    int censored;
+};
+
+int main(int argc, char **argv)
+{
+	printf("hello world\n");
+	return 0;
+}
